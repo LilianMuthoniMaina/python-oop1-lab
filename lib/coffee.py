0b7b6eb70eb1b58d21724102f8d91d45834bb30a
Lilian Muthoni <mainalilianm@gmail.comgit config --global --listnano ~/.gitconfiggit cofig user.namegit config --global user.email mainalilianm@gmail.com>
2026-02-23 13:07:26 +0300

    modified files

--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
 
 class Coffee:
-    pass
+    sizes = ["Small", "Medium", "Large"]
+
+    def __init__(self, size, price):
+        self._size = None
+        self.price = price
+        self.size = size
+
+    @property
+    def size(self):
+        return self._size
+    
+    @size.setter
+    def size(self, value):
+        if value not in self.sizes:
+            print("size must be Small, Medium, or Large")
+        else:
+            self._size = value
+
+
+    def tip(self):
+        print("This coffee is great, here’s a tip!")
+        self.price += 1
